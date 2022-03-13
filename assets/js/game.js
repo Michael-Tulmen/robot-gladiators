d70df22786000032fadb84ff4dfdb4306220e755
Michael Tulmen <michaeltulmen@gmail.com>
2022-03-13 13:26:42 -0500

    added more than one enemy in an array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//  * Fight all enemy-robots
+//  * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,11 +12,13 @@ if(playerHealth > 0) {
     console.log("Your player is still alive!");
 }
 
-var enemyName = "Roborto";
+//Array of enemy names
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+//fight function
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to Fight or Skip this battle? Enter 'FIGHT' or 'SKIP to choose.");
     if (promptFight === 'fight' || promptFight === 'FIGHT' || promptFight === 'Fight') {   
@@ -41,4 +49,8 @@ var fight = function() {
     }
 };
 
-fight();
+
+for(var i=0 ; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+//fight();
